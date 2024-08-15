6699e7fca6e833985bc14b38198804b4f8011cd6
Tharusha Nimsara <c.t.nimsara@gmail.com>
2024-08-15 22:31:56 +0530

    Login

---
+++ app/src/main/java/com/android/dogefoodie/MainActivity.java
@@ -0,0 +1,38 @@
+package com.android.dogefoodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.dogefoodie.admin.Admin_Add_Item;
+import com.android.dogefoodie.user.UserLogin;
+import com.android.dogefoodie.user.UserRegistration;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        Button btn2 = findViewById(R.id.btn4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserLogin.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Admin_Add_Item.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/android/dogefoodie/user/UserLogin.java
@@ -0,0 +1,71 @@
+package com.android.dogefoodie.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.dogefoodie.R;
+import com.android.dogefoodie.User;
+import com.android.dogefoodie.database.UserDB;
+
+import java.util.List;
+
+public class UserLogin extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private UserDB userDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.btn3);
+
+        userDB = new UserDB(this);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(UserLogin.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    boolean isAuthenticated = authenticateUser(email, password);
+                    if (isAuthenticated) {
+                        Toast.makeText(UserLogin.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getApplicationContext(), UserMain.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(UserLogin.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean authenticateUser(String email, String password) {
+        List<User> userList = userDB.getAllUsers();
+        for (User user : userList) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
